a2a9bdbefffe3b3fa5cdb2648e9bbecbe0abb9ba
Adeyemo Adekunle <adekunle@nestcoin.com>
2023-07-02 20:47:37 +0100

    feat : SignupRequest and MessageResponse Dtos

---
+++ auth-service/src/main/java/com/marketplace/auth/payload/request/SignupRequest.java
@@ -0,0 +1,33 @@
+package com.marketplace.auth.payload.request;
+
+import java.util.Set;
+
+import jakarta.validation.constraints.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class SignupRequest {
+	@NotBlank
+	@Size(min = 3, max = 20)
+	private String username;
+
+	@NotBlank
+	@Size(max = 50)
+	@Email
+	private String email;
+
+	private Set<String> role;
+
+	@NotBlank
+	@Size(min = 6, max = 50)
+	private String password;
+
+}
---
+++ auth-service/src/main/java/com/marketplace/auth/payload/response/MessageResponse.java
@@ -0,0 +1,17 @@
+package com.marketplace.auth.payload.request.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class MessageResponse {
+	private String message;
+
+}
